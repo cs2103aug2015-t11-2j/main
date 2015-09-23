ea608f7675cd51f206a8ecfdb1c87aab8e0ff698
Justin Erh <justinerh@gmail.com>
2015-09-23 17:09:19 +0800

    add Parser class
    added Parser class code to source

---
+++ src/main/Parser.java
@@ -0,0 +1,23 @@
+
+public class Parser {
+	
+	public String getAction(String userCommand){
+		String action = getFirstWord(userCommand).toLowerCase();
+		return action;
+	}
+	
+	public String getParameter(String userCommand){
+		String parameter = removeFirstWord(userCommand);
+		return parameter;
+	}
+	
+	private static String removeFirstWord(String userCommand) {
+		return userCommand.replace(getFirstWord(userCommand), "").trim();
+	}
+	
+	private static String getFirstWord(String userCommand) {
+		String commandTypeString = userCommand.trim().split("\\s+")[0];
+		return commandTypeString;
+	}
+	
+}
