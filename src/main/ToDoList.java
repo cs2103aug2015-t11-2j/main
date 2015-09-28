062f47dbc5888b561adb4b563c1c18d0051f33ad
dawnlord <290233090lhy@gmail.com>
2015-09-29 02:26:09 +0800

    add some messga and support the time and correct some formate

--- src/main/ToDoList.java
+++ src/main/ToDoList.java
@@ -1,7 +1,9 @@
 package main;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 
 public class ToDoList {
@@ -9,17 +11,19 @@ public class ToDoList {
 	private static final int MAX_NUMBER_OF_EVENTS = 1000;
 
 	private static final String EXIT_MSG = "Thanks for using Yui!";
-	private static final String WELCOME_MSG = "Hello, my master. This is Yui." + "\n" + "What would you like to do? \n";
-
+	private static final String WELCOME_MSG = " Hello, my master. Welcome back. This is Yui!" + "\n" + " What would you like to do? \n";
 	private static final String ERROR_MSG = "Error!";
+	private static final String SPACE = " ";
 	private static ArrayList<String> list;
-
+	private static final SimpleDateFormat DATAFORMAT = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+    private static String nowTime;
 	protected static boolean shouldExit = false;
 
 	public static String implement(String userCommand) throws IOException {
+			nowTime = DATAFORMAT.format(new Date()) + "\n";
 			String command = Parser.getAction(userCommand);
 			String parameter = Parser.getParameter(userCommand);
-			return modify(list, command, parameter);
+			return nowTime + SPACE + modify(list, command, parameter);
 	}
 
 	private static String modify(ArrayList<String> list, String command, String parameter) throws IOException {
@@ -54,7 +58,8 @@ public class ToDoList {
 	public static String initialize() throws IOException {
 		list = new ArrayList<String>(MAX_NUMBER_OF_EVENTS);
 		shouldExit = false;
-		return WELCOME_MSG;
+		nowTime = DATAFORMAT.format(new Date()) + "\n";
+		return nowTime + WELCOME_MSG;
 	}
 
 }
