23b1b5295b25dc907186965ca1eb225e7f4eb5ff
mao93cy <mao93cy@outlook.com>
2015-10-04 16:49:29 +0800

    update to support storage

--- src/main/ToDoList.java
+++ src/main/ToDoList.java
@@ -5,31 +5,33 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
-
 public class ToDoList {
 
-	private static final int MAX_NUMBER_OF_EVENTS = 1000;
+	// private static final int MAX_NUMBER_OF_EVENTS = 1000;
 
 	private static final String EXIT_MSG = "Thanks for using Yui!";
-	private static final String WELCOME_MSG = " Hello, my master. Welcome back. This is Yui!" + "\n" + " What would you like to do? \n";
+	private static final String WELCOME_MSG = " Hello, my master. Welcome back. This is Yui!" + "\n"
+			+ " What would you like to do? \n";
 	private static final String ERROR_MSG = "Error!";
 	private static final String SPACE = " ";
+	private static Storage s;
 	private static ArrayList<String> list;
 	private static final SimpleDateFormat DATAFORMAT = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
-    	private static String nowTime;
+	private static String nowTime;
 	protected static boolean shouldExit = false;
 
 	public static String implement(String userCommand) throws IOException {
-			nowTime = DATAFORMAT.format(new Date()) + "\n";
-			String command = Parser.getAction(userCommand);
-			String parameter = Parser.getParameter(userCommand);
-			return nowTime + SPACE + modify(list, command, parameter);
+		nowTime = DATAFORMAT.format(new Date()) + "\n";
+		String command = Parser.getAction(userCommand);
+		String parameter = Parser.getParameter(userCommand);
+		return nowTime + SPACE + modify(s, list, command, parameter);
 	}
 
-	private static String modify(ArrayList<String> list, String command, String parameter) throws IOException {
+	private static String modify(Storage s, ArrayList<String> list, String command, String parameter)
+			throws IOException {
 		switch (command) {
 		case "add": {
-			return Action.addToList(list, parameter);
+			return Action.addToList(s, list, parameter);
 
 		}
 		case "show": {
@@ -40,14 +42,14 @@ public class ToDoList {
 		}
 		case "delete": { // need to decide when to backup and which actions need
 							// backup
-			return Action.deleteEvent(list, parameter);
+			return Action.deleteEvent(s, list, parameter);
 
 		}
 		case "search": {
 			return Action.searchKey(list, parameter);
 		}
 		case "update": {
-			return Action.update(list, parameter);
+			return Action.update(s, list, parameter);
 		}
 		case "exit": {
 			Action.exit();
@@ -58,11 +60,12 @@ public class ToDoList {
 		}
 		}
 
-
 	}
 
 	public static String initialize() throws IOException {
-		list = new ArrayList<String>(MAX_NUMBER_OF_EVENTS);
+		s = new Storage("Yui");
+		list = s.load(s.mainDir);
+		// list = new ArrayList<String>(MAX_NUMBER_OF_EVENTS);
 		shouldExit = false;
 		nowTime = DATAFORMAT.format(new Date()) + "\n";
 		return nowTime + WELCOME_MSG;
