a1c8269644742fa277624d3f882344da7bf1a701
Justin Erh <justinerh@gmail.com>
2015-10-08 11:38:55 +0800

    Arraylist getParameter
    Reworking of getParameter to return an ArrayList.

--- src/main/Parser.java
+++ src/main/Parser.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.*;
+
 public class Parser {
 	
 	public static String getAction(String userCommand){
@@ -7,24 +9,37 @@ public class Parser {
 		return action;
 	}
 	
-	public static String getParameter(String userCommand){
+	/*public static String getParameter(String userCommand){
 		String parameter = removeFirstWord(userCommand);
 		return parameter;
-	}
+	}*/
 	
-	/*public static ArrayList<String> getParameter(String userCommand){
-		String action = getFirstWord(userCommand).toLowerCase();
-		ArrayList<String> parameter = new ArrayList<String>;
+	public static ArrayList<String> getParameter(String userCommand){
+		String action = getAction(userCommand);
+		ArrayList<String> parameter = new ArrayList<String>();
 		if (action.equals("add")){
 			String unsplitParameter = removeFirstWord(userCommand);
-			String event = getFirstWord()
+			if (unsplitParameter.contains("from")){ //event with specific time interval
+				String[] parameters = unsplitParameter.split("from |to ");
+				parameter.add(parameters[0]); //event
+				parameter.add(parameters[1]); //start time
+				parameter.add(parameters[2]); //end time
+				parameter.add(parameters[3]); //date
+			}
+			
+			else if (unsplitParameter.contains("by")){ //deadline
+				String[] parameters = unsplitParameter.split("by ");
+				parameter.add(parameters[0]); //event
+				parameter.add(parameters[1]); //time
+				parameter.add(parameters[2]); //date
+			}
 		}
 		else{
-			parameter.add(removeFirstWord(userCommand));
+			parameter.add(removeFirstWord(userCommand)); //no time specified
 		}
 		
 		return parameter;
-	}*/
+	}
 	
 	
 	private static String removeFirstWord(String userCommand) {
