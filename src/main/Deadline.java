2c15d40173a27afe3a695ca1b5c6f9bce5f15b9e
mao93cy <mao93cy@outlook.com>
2015-10-09 15:15:45 +0800

    create deadline class

---
+++ src/main/Deadline.java
@@ -0,0 +1,23 @@
+package main;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class Deadline {
+	private Time time;
+	private Date date;
+
+	// constructor
+	Deadline(Time time, Date date) {
+		this.time = time;
+		this.date = date;
+	}
+
+	Time getTime() {
+		return this.time;
+	}
+
+	Date getDate() {
+		return this.date;
+	}
+}
