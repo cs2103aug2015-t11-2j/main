0abb7216be051765caf77563dcab4d73dbe5f1fb
Le Nguyen <nguyenle1994.nl@gmail.com>
2015-10-18 03:54:16 +0800

    Revert "Revert "update code format of GUI""
    This reverts commit 736c1419b0fa3954fe2fd9c116ba0b23631ac48e.

--- src/Yui/Yui_GUI.java
+++ src/Yui/Yui_GUI.java
@@ -21,8 +21,8 @@ import main.ToDoList;
 public class Yui_GUI extends Application{
 	protected static String userCommand;
 	protected static String returnCommand;
-	private double xOffset = 0;
-    private double yOffset = 0;
+	protected static double xOffset = 0;
+	protected static double yOffset = 0;
 
 
    public static void main(String[] args) {
@@ -35,16 +35,24 @@ public class Yui_GUI extends Application{
        primaryStage.initStyle(StageStyle.UNDECORATED);
        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
        primaryStage.show();
-
+       
+       //Set main grid
        GridPane grid = new GridPane();
        grid.setHgap(5);
        grid.setVgap(5);
        grid.setPadding(new Insets(10, 5, 5, 25));
-
+       
+       //set background
        Image background = new Image(getClass().getResourceAsStream("uiground.png"));
        ImageView bk = new ImageView(background);
-
-
+       Group backg = new Group();
+       backg.getChildren().addAll(bk,grid);
+       
+       //set icon
+       Image iconYui = new Image(getClass().getResourceAsStream("logo.png"));
+       grid.add(new ImageView(iconYui), 0, 0);
+       
+       //set exit button
        Image imageExit = new Image(getClass().getResourceAsStream("exit.png"));
        ImageView exit = new ImageView(imageExit);
        exit.setOnMouseClicked(new EventHandler<MouseEvent>(){
@@ -54,51 +62,39 @@ public class Yui_GUI extends Application{
     		   }
     	   });
        grid.add(exit, 4, 0);
-
-       Image iconYui = new Image(getClass().getResourceAsStream("logo.png"));
-       grid.add(new ImageView(iconYui), 0, 0);
-
+       
+       //set menu button
        Image menu1 = new Image(getClass().getResourceAsStream("menu.png"));
        grid.add(new ImageView(menu1), 4, 2);
 
+       //set enter button
        Image enter = new Image(getClass().getResourceAsStream("ok.png"));
        ImageView enterKey = new ImageView(enter);
        grid.add(enterKey, 4, 3);
+       
+       //Control the dragging of stage
+       DragController.dragStage(grid, primaryStage);
 
-       grid.setOnMousePressed(new EventHandler<MouseEvent>() {
-           @Override
-           public void handle(MouseEvent event) {
-               xOffset = event.getSceneX();
-               yOffset = event.getSceneY();
-           }
-       });
-
-       grid.setOnMouseDragged(new EventHandler<MouseEvent>() {
-           @Override
-           public void handle(MouseEvent event) {
-               primaryStage.setX(event.getScreenX() - xOffset);
-               primaryStage.setY(event.getScreenY() - yOffset);
-           }
-       });
-
-       Group backg = new Group();
-       backg.getChildren().addAll(bk,grid);
-
+       //set the scene
        Scene scene = new Scene(backg, 600, 400);
        primaryStage.setScene(scene);
 
+       //add text box to show message
        final TextArea showBox = new TextArea();
        showBox.setPrefSize(490, 275);
        grid.add(showBox, 0, 2);
        showBox.setEditable(false);
 
+       //add command box
        final TextField userCommandBox = new TextField();
        grid.add(userCommandBox, 0, 3);
        userCommandBox.requestFocus();
-
+       
+       //initialize GUI and Logic
        returnCommand = ToDoList.initialize();
        showBox.appendText(returnCommand + "\n");
-
+       
+       //catch the motion of users
        MotionCatcher.keyboardCatcher(userCommandBox, showBox);
        MotionCatcher.mouseCatcher(enterKey, userCommandBox, showBox);
    }
