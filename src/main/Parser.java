27aa28749f674c63a3232e3b639213af2868d87a
Le Nguyen <nguyenle1994.nl@gmail.com>
2015-10-20 22:01:21 +0800

    parser support for event

--- src/main/Parser.java
+++ src/main/Parser.java
@@ -1,74 +1,94 @@
 package main;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Parser {
-	
-	public static String getAction(String userCommand){
+
+	public static String getAction(String userCommand) {
 		String action = Splitter.getFirstWord(userCommand).toLowerCase();
 		return action;
 	}
-	
-	/*public static String getParameter(String userCommand){
-		String parameter = Splitter.removeFirstWord(userCommand);
-		return parameter;
-	}*/
-	
-	public static ArrayList<String> getParameter(String userCommand){
+
+	/*
+	 * public static String getParameter(String userCommand){ String parameter =
+	 * Splitter.removeFirstWord(userCommand); return parameter; }
+	 */
+
+	public static ArrayList<String> getParameter(String userCommand) {
 		String action = getAction(userCommand);
 		ArrayList<String> parameter = new ArrayList<String>();
-		if (action.equals("add")){
+		if (action.equals("add")) {
 			String unsplitParameter = Splitter.removeFirstWord(userCommand);
-			if (unsplitParameter.contains("from")){ //event with specific time interval
+			if (unsplitParameter.contains("from")) { // event with specific time
+														// interval
 				parameter = Splitter.splitEvent(parameter, unsplitParameter);
 			}
-			
-			else if (unsplitParameter.contains("by")){ //deadline
+
+			else if (unsplitParameter.contains("by")) { // deadline
 				parameter = Splitter.splitDeadline(parameter, unsplitParameter);
-			}
-			else{//no time specified
+			} else {// no time specified
 				parameter.add(Splitter.removeFirstWord(userCommand));
 			}
+		} else {
+			parameter.add(Splitter.removeFirstWord(userCommand)); // not an
+																	// "add"
+																	// event
 		}
-		else{
-			parameter.add(Splitter.removeFirstWord(userCommand)); //not an "add" event
-		}
-		
+
 		return parameter;
 	}
-		
+
 	public static int getUpdateIndex(ArrayList<String> parameter) {
-		return Integer.valueOf(parameter.get(0).substring(0, parameter.indexOf(" ")))-1;
+		return Integer.valueOf(parameter.get(0).substring(0, parameter.indexOf(" "))) - 1;
 	}
 
 	public static Event getUpdateEvent(ArrayList<String> parameter) {
 		String updateParameter = parameter.get(0).substring(parameter.indexOf(" ") + 1);
-		
-		return null;		
+
+		return null;
 	}
 
-	public static Event parseForEvent(ArrayList<String> parameter) {
-		if (parameter.size() == 1){
+	public static Event parseForEvent(ArrayList<String> parameter) throws ParseException {
+		if (parameter.size() == 1) {
 			return new Event(parameter.get(0));
-		} else if (parameter.size() == 2){
-			return new Event(parameter.get(0), parseForDeadline(parameter.get(1)));
-		} else if (parameter.size() == 3){
-			return new Event(parameter.get(0), parseForEventTime(parameter.get(1), parameter.get(2)));
+		} else if (parameter.size() == 2) {
+			return new Event(parameter.get(0), parseForDeadline(parameter.get(1), parameter.get(2)));
+		} else if (parameter.size() == 3) {
+			return new Event(parameter.get(0), parseForEventTime(parameter.get(1), parameter.get(2),parameter.get(3)));
 		}
 		return null;
 	}
 
-	private static EventTime parseForEventTime(String string, String string2) {
-		return new EventTime(parseForCalendar(string),parseForCalendar(string2));
+	private static EventTime parseForEventTime(String start, String end, String date) throws ParseException {
+		Calendar cal = Calendar.getInstance();// default today
+		if (date.equalsIgnoreCase("tomorrow")){
+			cal.add(Calendar.DATE, 1);
+		}
+		else{
+			SimpleDateFormat dateF = new SimpleDateFormat("dd/mm/yyyy");
+			cal.setTime(dateF.parse(date));
+		}
+		
+		return new EventTime(parseForCalendarTime(start, cal),parseForCalendarTime(end, cal));
 	}
 
-	private static Deadline parseForDeadline(String string) {
-		return new Deadline(parseForCalendar(string));
+	private static Deadline parseForDeadline(String deadline, String date) throws ParseException {
+		Calendar cal = Calendar.getInstance();// default today
+		if (date.equalsIgnoreCase("tomorrow")){
+			cal.add(Calendar.DATE, 1);
+		}
+		else{
+			SimpleDateFormat dateF = new SimpleDateFormat("dd/mm/yyyy");
+			cal.setTime(dateF.parse(date));
+		}
+		return new Deadline(parseForCalendarTime(deadline,cal));
 	}
 
-	private static Calendar parseForCalendar(String string) {
-		Calendar cal = Calendar.getInstance();
-		cal.set(Calendar.HOUR_OF_DAY, value);
-		return null;
+	private static Calendar parseForCalendarTime(String time, Calendar cal) throws ParseException {
+		SimpleDateFormat dateF = new SimpleDateFormat("HH:mm");
+		cal.setTime(dateF.parse(time));
+		return cal;
 	}
 }
