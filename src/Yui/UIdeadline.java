90511e3a5612253b7f94b4515d143edeb007eeb5
dawnlord <290233090lhy@gmail.com>
2015-10-29 00:37:26 +0800

    delete useless

--- src/Yui/UIdeadline.java
+++ src/Yui/UIdeadline.java
@@ -11,7 +11,6 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import main.Deadline;
 import main.Event;
