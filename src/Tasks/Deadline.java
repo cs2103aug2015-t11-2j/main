bdc32222a81ad9b48cf132d21151538ff31b1af1
Mao Chenyang <mao93cy@outlook.com>
2015-11-08 23:43:27 +0800

    remove unused code

--- src/Tasks/Deadline.java
+++ src/Tasks/Deadline.java
@@ -1,32 +1,4 @@
 //@@author A0127142R
-/*
-package main;
-
-import java.sql.Date;
-import java.sql.Time;
-
-//unused
-
-
-public class Deadline {
-	private Time time;
-	private Date date;
-
-	// constructor
-	Deadline(Time time, Date date) {
-		this.time = time;
-		this.date = date;
-	}
-
-	public Time getTime() {
-		return this.time;
-	}
-
-	public Date getDate() {
-		return this.date;
-	}
-}
-*/
 
 package Tasks;
 
