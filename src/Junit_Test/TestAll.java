b572debcbe11b61599beb55be7a9cd5132ee2315
dawnlord <290233090lhy@gmail.com>
2015-11-09 16:55:12 +0800

    update test

--- src/Junit_Test/TestAll.java
+++ src/Junit_Test/TestAll.java
@@ -3,11 +3,8 @@ package Junit_Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-//import controller.LogicTest;
-//import view.UiTest;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-   //UiTest.class,
    ActionTest.class,
    SUTStorage.class,
    SUTParser_LHY.class,
